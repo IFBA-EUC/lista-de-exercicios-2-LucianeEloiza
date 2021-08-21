257a54e65f8a811cd66cfecc0ff75691d84c831e
LucianeEloiza <88690088+LucianeEloiza@users.noreply.github.com>
2021-08-21 14:14:52 -0300

    questão 3

---
+++ questao3.java
@@ -0,0 +1,31 @@
+ 
+
+// o Erro está no codigo "distancia = Math.pow(Math.pow(x2-x1, 2) + Math.pow(y2-y1, 2), 1/2)" 
+// a onde o calculo está sendo por 1/2, isso está fazendo com que retorne um valor inteiro
+// e por isso sempre a distância é 1.0
+ 
+// para corrigir o codigo é somente trocar o 1/2 por 2, veja a seguir:
+
+import java.util.Scanner;
+public class questao3 {
+ public static void main(String[] args) {
+ Scanner teclado = new Scanner(System.in);
+ double x1, y1, x2, y2, distancia;
+ 
+ System.out.println("Entre com as coordenadas x e y dos pontos nesta ordem:");
+ x1 = teclado.nextFloat();
+ y1 = teclado.nextFloat();
+ x2 = teclado.nextFloat();
+ y2 = teclado.nextFloat();
+ 
+ distancia = Math.pow(Math.pow(x2-x1, 2) + Math.pow(y2-y1, 2), 2);
+ System.out.println("A distância é: " + distancia);
+ }
+}
+
+	 
+	 
+	 
+	   
+
+
