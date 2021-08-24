e1c4a1f541b554a16eff21c75fe2dcb089d08302
LucianeEloiza <88690088+LucianeEloiza@users.noreply.github.com>
2021-08-23 21:51:38 -0300

    Add files via upload
    questão 5

---
+++ questao5.java
@@ -0,0 +1,17 @@
+
+public class questao5 {
+
+	public static void main(String[] args) {
+		int y;
+		
+		
+		 for( y = 0; y <= 10; y++) {
+			 System.out.println("tabuada de " + y); 
+		 for(int n = 0; n <= 10; n++) {
+			System.out.println (y+ "X" + n + " =" + (y*n) ); 
+		 }
+		
+	}
+
+}
+ }
